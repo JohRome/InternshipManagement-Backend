0a124bed5569772621cc06be273b6e87cb8376d6
Johan Romeo <jofiluren@outlook.com>
2024-02-21 21:28:16 +0100

    simple mockito test to see how GitHub Actions respond

---
+++ src/test/java/com/jromeo/internshipbackend/service/InternshipServiceTest.java
@@ -0,0 +1,56 @@
+package com.jromeo.internshipbackend.service;
+
+import com.jromeo.internshipbackend.dto.InternshipDTO;
+import com.jromeo.internshipbackend.entity.Internship;
+import com.jromeo.internshipbackend.repository.InternshipRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class InternshipServiceTest {
+    @InjectMocks
+    private InternshipService internshipService;
+    @Mock
+    private InternshipRepository internshipRepository;
+    @Mock
+    private ModelMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void should_create_internship() {
+        // Given
+        InternshipDTO dto = new InternshipDTO(
+                1,
+                "Company",
+                "Description",
+                "Contact Person",
+                true
+        );
+        Internship internship = new Internship(
+                1,
+                "Company",
+                "Description",
+                "Contact Person",
+                true
+        );
+        when(mapper.map(dto, Internship.class)).thenReturn(internship);
+        when(mapper.map(internship, InternshipDTO.class)).thenReturn(dto);
+        when(internshipRepository.save(internship)).thenReturn(internship);
+
+        // When
+        InternshipDTO createdInternship = internshipService.createInternship(dto);
+
+        // Then
+        assertEquals(dto.getId(), createdInternship.getId());
+    }
+}
